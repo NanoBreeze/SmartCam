0f27664b6b68a5ba0b4a68bd51c7ee594fa1deda
Lenny Cheng <NanoBreeze@users.noreply.github.com>
2020-06-17 21:24:08 -0400

    Added wifi components to rootfs build and let rcS connect to wlan0 on startup

--- scripts/assemble_rootfs.sh
+++ scripts/assemble_rootfs.sh
@@ -3,14 +3,14 @@
 
 ROOTFS=../rootfs/
 
+mkdir -p ${ROOTFS}/dev ${ROOTFS}/proc ${ROOTFS}/sys ${ROOTFS}/boot ${ROOTFS}/etc ${ROOTFS}/usr ${ROOTFS}/lib ${ROOTFS}/tmp ${ROOTFS}/var || true
+mkdir -p ${ROOTFS}/usr/share || true
+
 cp -r ../busybox-1.31.1/install_rootfs/* ${ROOTFS}
 rsync -a --ignore-existing ../tools/armv8-rpi3-linux-gnueabihf/armv8-rpi3-linux-gnueabihf/sysroot/ ${ROOTFS} #I suppose we could also use cp, but I was curious how to use rsync --ignore-existing may not actually be needed
 cp ../tools/armv8-rpi3-linux-gnueabihf/armv8-rpi3-linux-gnueabihf/debug-root/usr/bin/* ${ROOTFS}/bin
 
-mkdir -p ${ROOTFS}/dev ${ROOTFS}/proc ${ROOTFS}/sys ${ROOTFS}/boot ${ROOTFS}/etc ${ROOTFS}/usr ${ROOTFS}/lib || true
-mkdir -p ${ROOTFS}/usr/share || true
-
-cp -r ../scripts/init.d ${ROOTFS}/etc/init.d || true
+cp -r ../scripts/init.d/ ${ROOTFS}/etc/ || true
 cp -r ../scripts/udhcpc_scripts/ ${ROOTFS}/usr/share/udhcpc/ || true
 
 cp ../scripts/inittab ${ROOTFS}/etc || true
@@ -27,5 +27,16 @@ cp -r ../bin/crda/ ${ROOTFS}/lib/
 
 # Copy libnl and iw
 cp -r ../libnl-3.2.25/install/sbin/* ${ROOTFS}/sbin/
-cp -r ../libnl-3.2.25/install/lib/* ${ROOTFS}/lib
-cp ../iw-5.4/iw ${ROOTFS}/sbin
+cp -r ../libnl-3.2.25/install/lib/* ${ROOTFS}/lib/
+cp ../iw-5.4/iw ${ROOTFS}/sbin/
+
+# Copy Openssl files
+cp ../openssl-1.1.1g/install/bin/* ${ROOTFS}/bin/
+cp -r ../openssl-1.1.1g/install/lib/* ${ROOTFS}/lib/
+cp -r ../openssl-1.1.1g/install/ssl/ ${ROOTFS}/etc/
+
+# Copy wpa_supplicant, wpa_passphrase, wpa_cli, and wpa_supplicant.conf, which contains hashed pre-shared key for whitebyer_2.4ghz 
+cp ../wpa_supplicant-2.9/wpa_supplicant/wpa_cli ${ROOTFS}/bin
+cp ../wpa_supplicant-2.9/wpa_supplicant/wpa_passphrase ${ROOTFS}/bin
+cp ../wpa_supplicant-2.9/wpa_supplicant/wpa_supplicant ${ROOTFS}/bin
+cp -r ../scripts/wpa_supplicant/ ${ROOTFS}/etc/
