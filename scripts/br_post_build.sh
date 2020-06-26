7008a11ef1ee92c4b94e7cf35fe6517fea6db842
Lenny Cheng <NanoBreeze@users.noreply.github.com>
2020-06-26 19:45:57 -0400

    Stamp git commit id into a file in rootfs

--- scripts/br_post_build.sh
+++ scripts/br_post_build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # Post-build script for build-root stuff
 
-echo "BLOOOP"
-echo "About to chmod"
-echo $?
-chmod 755 $TARGET_DIR/bin/busybox
-echo $HOST_DIR
-echo $STAGING_DIR
+#echo "BLOOOP"
+#echo "About to chmod"
+#echo $?
+#chmod 755 $TARGET_DIR/bin/busybox
+#echo $HOST_DIR
+#echo $STAGING_DIR
 
-echo "echoing done"
+#echo "echoing done"
+
+# create a file containing the git commit this build was created from
+git rev-parse HEAD > $TARGET_DIR/build_id.txt
